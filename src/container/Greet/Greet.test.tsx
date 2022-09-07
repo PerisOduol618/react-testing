bd09865fd5e071cc859d0f780237069297746462
PerisOduol618 <oduolpepe618@gmail.com>
2022-09-07 16:24:03 +0300

    Nest describe

--- src/container/Greet/Greet.test.tsx
+++ src/container/Greet/Greet.test.tsx
@@ -1,18 +1,18 @@
-import { render, screen} from "@testing-library/react"
-import Greet from "./Greet"
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
 
-describe ("Greet", () => {
-test("Greet renders correctly", () => {
-      render(<Greet/>)
-      const textElement = screen.getByText(/hello/i)
-      expect ( textElement ).toBeInTheDocument()
-})
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-      render(<Greet name= "Peris"/>)
-      const textElement = screen.getByText("Hello Peris")
-      expect ( textElement ).toBeInTheDocument()
-
-})
-
-})
+  describe("Nested", () => {
+    test("Greet renders with a name", () => {
+      render(<Greet name="Peris" />);
+      const textElement = screen.getByText("Hello Peris");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
