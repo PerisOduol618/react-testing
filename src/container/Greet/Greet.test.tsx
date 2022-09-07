45fcddbb57c4ec41e8ddb1d985bd15e7807a3965
PerisOduol618 <oduolpepe618@gmail.com>
2022-09-07 16:22:15 +0300

    Add describe to the to group tests

--- src/container/Greet/Greet.test.tsx
+++ src/container/Greet/Greet.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen} from "@testing-library/react"
 import Greet from "./Greet"
 
+describe ("Greet", () => {
 test("Greet renders correctly", () => {
       render(<Greet/>)
       const textElement = screen.getByText(/hello/i)
@@ -11,5 +12,7 @@ test("Greet renders with a name", () => {
       render(<Greet name= "Peris"/>)
       const textElement = screen.getByText("Hello Peris")
       expect ( textElement ).toBeInTheDocument()
+
+})
 
 })
