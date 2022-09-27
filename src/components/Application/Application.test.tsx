9d211a435512cfa81cf82b6c9549ce51371a4496
PerisOduol618 <oduolpepe618@gmail.com>
2022-09-27 17:14:07 +0300

    Check a combobox

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -8,6 +8,12 @@ describe( "Application", () => {
             const nameElement = screen.getByRole('textbox')
             expect(nameElement).toBeInTheDocument()
 
+            const jobLocationElement = screen.getByRole('combobox')
+            expect(jobLocationElement).toBeInTheDocument()
+
+            const termsElement = screen.getByRole('checkbox')
+            expect(termsElement).toBeInTheDocument()
+
       });
 
 });
